6446632b24c6274ae7f2ccfa549d4577a0363899
Ben Braunstein <benzbraunstein@gmail.com>
2019-07-15 18:43:48 -0400

    Done.

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,34 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+
+  def initialize(text = nil)
+    @text = text
+  end
+
+  def word_count
+    @text.split.length
+  end
+
+  def letter_count
+    @text.length
+  end
+
+  def vowel_count
+    total = 0
+    vowels = "aeiou"
+
+    vowels.length.times do |i|
+      total += @text.count(vowels[i])
+    end
+    total + 1
+  end
+
+  def consonant_count
+    return self.letter_count - self.vowel_count - self.word_count + 1
+  end
+
+  def most_common_letter
+    
+  end
+end
